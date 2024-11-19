03f9e247947e5847a18bc09508d8b068266317a1
torddey <ea.torddey@gmail.com>
2024-11-19 21:10:34 +0000

    Using whitespaces

--- Program.cs
+++ Program.cs
@@ -372,7 +372,7 @@ fraudulentOrderIDs[2] = "C789";
 //    } 
 // }
 
-//NAMING CONVENTIONS
+//NAMING CONVENTIONS(COMMENTING)
 //A local variable is a variable that is scoped within the body of a method
 
 /*
@@ -381,19 +381,87 @@ fraudulentOrderIDs[2] = "C789";
   consist of a letter from A to E, and a three
   digit number. Ex. A123.
 */
-Random random = new Random();
-string[] orderIDs = new string[5];
+// Random random = new Random();
+// string[] orderIDs = new string[5];
 
-for (int i = 0; i < orderIDs.Length; i++)
+// for (int i = 0; i < orderIDs.Length; i++)
+// {
+//     int prefixValue = random.Next(65, 70);
+//     string prefix = Convert.ToChar(prefixValue).ToString();
+//     string suffix = random.Next(1, 1000).ToString("000");
+
+//     orderIDs[i] = prefix + suffix;
+// }
+
+// foreach (var orderID in orderIDs)
+// {
+//     Console.WriteLine(orderID);
+// }
+
+// WHITESPACES
+// Random dice = new Random();
+
+// int roll1 = dice.Next(1, 7);
+// int roll2 = dice.Next(1, 7);
+// int roll3 = dice.Next(1, 7);
+
+// int total = roll1 + roll2 + roll3;
+// Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+// if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) 
+// {
+//     if ((roll1 == roll2) && (roll2 == roll3)) 
+//     {
+//         Console.WriteLine("You rolled triples!  +6 bonus to total!");
+//         total += 6; 
+//     } 
+//     else 
+//     {
+//         Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+//         total += 2;
+//     }
+// }
+
+
+// CHALLENGE
+string str = "The quick brown fox jumps over the lazy dog.";
+// convert the message into a char array
+char[] charMessage = str.ToCharArray();
+// Reverse the chars
+Array.Reverse(charMessage);
+int x = 0;
+// count the o's
+foreach (char i in charMessage) { if (i == 'o') { x++; } }
+// convert it back to a string
+string new_message = new String(charMessage);
+// print it out
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
+
+
+//RESULT
+/*
+   This code reverses a message, counts the number of times 
+   a particular character appears, then prints the results
+   to the console window.
+ */
+
+string originalMessage = "The quick brown fox jumps over the lazy dog.";
+
+char[] message = originalMessage.ToCharArray();
+Array.Reverse(message);
+
+int letterCount = 0;
+
+foreach (char letter in message)
 {
-    int prefixValue = random.Next(65, 70);
-    string prefix = Convert.ToChar(prefixValue).ToString();
-    string suffix = random.Next(1, 1000).ToString("000");
-
-    orderIDs[i] = prefix + suffix;
+    if (letter == 'o')
+    {
+        letterCount++;
+    }
 }
 
-foreach (var orderID in orderIDs)
-{
-    Console.WriteLine(orderID);
-}
+string newMessage = new String(message);
+
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {letterCount} times.");
