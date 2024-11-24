0df0bff31db3e9cb27da5c223e05c9bc910a596c
torddey <ea.torddey@gmail.com>
2024-11-24 20:13:03 +0000

    Variable scope

--- Program.cs
+++ Program.cs
@@ -629,35 +629,97 @@ using System;
 // Console.WriteLine(!pangram.Contains("fox"));
 
 // int saleAmount = 1001;
-// // int discount = saleAmount > 1000 ? 100 : 50;
+// // int discount = saleAmount > 1000 ? 100 : 50; conditional operator
 // Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
 
-string permission = "Admin|Manager";
-int level = 53;
+// string permission = "Admin|Manager";
+// int level = 56;
 
-if (permission.Contains("Admin"))
+// if (permission.Contains("Admin"))
+// {
+//     if (level > 55)
+//     {
+//         Console.WriteLine("Welcome, Super Admin user.");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Welcome, Admin user.");
+//     }
+// }
+// else if (permission.Contains("Manager"))
+// {
+//     if (level >= 20)
+//     {
+//         Console.WriteLine("Contact an Admin for access.");
+//     }
+//     else
+//     {
+//         Console.WriteLine("You do not have sufficient privileges.");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("You do not have sufficient privileges.");
+// }
+
+// VARIABLE SCOPE & LOGIC IN CODE BLOCK
+// Creating variable in a code block
+
+// bool flag = true;
+// int value = 0;
+
+// if (flag)
+// {
+//   Console.WriteLine($"Inside the code block: {value}");
+// }
+
+// value = 10;
+// Console.WriteLine($"Outside the code block: {value}");
+
+ 
+//Incorrect format
+/*
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+
+foreach (int number in numbers)
 {
-    if (level > 55)
+    int total;
+
+    total += number;
+
+    if (number == 42)
     {
-        Console.WriteLine("Welcome, Super Admin user.");
+       bool found = true;
+
     }
-    else
-    {
-        Console.WriteLine("Welcome, Admin user.");
-    }
+
 }
-else if (permission.Contains("Manager"))
+
+if (found) 
 {
-    if (level >= 20)
-    {
-        Console.WriteLine("Contact an Admin for access.");
-    }
-    else
-    {
-        Console.WriteLine("You do not have sufficient privileges.");
-    }
+    Console.WriteLine("Set contains 42");
+
 }
-else
+
+Console.WriteLine($"Total: {total}");
+*/
+
+//Correct Format
+/*
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+int total = 0;
+bool found = false;
+
+foreach (int number in numbers)
 {
-    Console.WriteLine("You do not have sufficient privileges.");
+    total += number;
+    if (number == 42)
+        found = true;
 }
+
+if (found)
+    Console.WriteLine("Set contains 42");
+
+Console.WriteLine($"Total: {total}");
+*/
+
--- obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7ef5ccba5dae845848cbdf480002b2c85347774")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ac0d0fe1b26a79a9b268755bf6bce1b9a581663")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
